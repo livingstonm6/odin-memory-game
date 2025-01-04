7a4ebf65952fc28490a37f7e27c4780b9c2400bc
livingstonm6 <livingstonm6@mymacewan.ca>
2025-01-04 16:11:33 -0700

    Implemented header.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+    const [score, setScore] = useState(0)
+
 
   return (
     <>
-        <h1>Memory Game</h1>
+        <div className="header">
+            <div className="header-left">
+                <h4>Memory Game</h4>
+                <p>Earn points by clicking on a Pokemon, but don&#39;t click on the same one twice!</p>
+            </div>
+            <div className="header-right">
+                <p>Score: {score}</p>
+            </div>
+        </div>
+
     </>
   )
 }
