06947b4b7d8da676b86c790178afb9b388bd9384
livingstonm6 <livingstonm6@mymacewan.ca>
2025-01-06 19:24:51 -0700

    Moved API request from useEffect in App component to the main script.

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,31 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const urls = [
+    'https://pokeapi.co/api/v2/pokemon/1',
+    'https://pokeapi.co/api/v2/pokemon/2',
+    'https://pokeapi.co/api/v2/pokemon/3',
+    'https://pokeapi.co/api/v2/pokemon/4',
+    'https://pokeapi.co/api/v2/pokemon/5',
+    'https://pokeapi.co/api/v2/pokemon/6',
+    'https://pokeapi.co/api/v2/pokemon/7',
+    'https://pokeapi.co/api/v2/pokemon/8',
+];
+
+Promise.all(
+    [fetch(urls[0]).then(res => res.json()),
+        fetch(urls[1]).then(res => res.json()),
+        fetch(urls[2]).then(res => res.json()),
+        fetch(urls[3]).then(res => res.json()),
+        fetch(urls[4]).then(res => res.json()),
+        fetch(urls[5]).then(res => res.json()),
+        fetch(urls[6]).then(res => res.json()),
+        fetch(urls[7]).then(res => res.json()),
+    ]).then(res => createRoot(document.getElementById('root')).render(
+        <StrictMode>
+            <App pokemonData={res}/>
+        </StrictMode>,
+    )
+    )
+    .catch(err => console.log(err));
+
